6da44335e8333cd7fd02df103cce0bf5553bbdb1
chirag-trivedi <chirag.trivedi@gmail.com>
2021-10-30 10:23:14 -0400

    adding user forms

--- taskmate/urls.py
+++ taskmate/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todolist/',include('todolist.urls'))
+    path('todolist/',include('todolist.urls')),
+    path('account/',include('users.urls')),
 ]
